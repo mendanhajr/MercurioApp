be956b51f4f48cb6e45191ac47b0f5baa1688c8f
Mendanha <luizmendanhajr@gmail.com>
2021-09-08 14:16:20 -0300

    removido letra do titulo

--- src/Container/Dashboard/Dashboard.js
+++ src/Container/Dashboard/Dashboard.js
@@ -24,7 +24,7 @@ function optionsHeader(){
     const {theme} = useTheme();
     return(
         {
-            headerTitle: 'MERCURIO',
+            headerTitle: 'MERCURI',
             headerTitleAlign: 'center',
             headerStyle: {
                 backgroundColor: theme.colors.primary,
