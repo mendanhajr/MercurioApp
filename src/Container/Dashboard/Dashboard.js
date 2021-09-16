cac23a37c6d22ae63c2862d9dc0d9af58ef80d8c
Mendanha <luizmendanhajr@gmail.com>
2021-09-16 14:51:02 -0300

    alterado titulo do menu do app

--- src/Container/Dashboard/Dashboard.js
+++ src/Container/Dashboard/Dashboard.js
@@ -24,7 +24,7 @@ function optionsHeader(){
     const {theme} = useTheme();
     return(
         {
-            headerTitle: 'MERCURI',
+            headerTitle: 'TESTE',
             headerTitleAlign: 'center',
             headerStyle: {
                 backgroundColor: theme.colors.primary,
